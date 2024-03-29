ba12d3968c075e643ce23e897a275f1dae0c98b9
jean <jean__100limites@hotmail.com>
2024-03-28 22:56:36 -0300

    formatando a classe Calculadora.java

--- src/main/java/org/study/calculadora/Calculadora.java
+++ src/main/java/org/study/calculadora/Calculadora.java
@@ -2,18 +2,18 @@ package org.study.calculadora;
 
 public class Calculadora {
     public static Double calculadora(OperacoesEnum operacao, Double a, Double b){
-        switch (operacao){
+        switch (operacao) {
             case SOMA -> {
                 return a + b;
             }
             case SUBTRACAO -> {
-                return a -b;
+                return a - b;
             }
             case DIVISAO -> {
-                return a/b;
+                return a / b;
             }
             case MULTIPLICACAO -> {
-                return a*b;
+                return a * b;
             }
             default -> throw new IllegalArgumentException("operação desconhecida");
         }
